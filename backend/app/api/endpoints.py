fde3e81806ce82a9911988caa6234aa05602f891
Sergio Paulo Villegas Flores <sergiopvf@MacBook-Pro-de-Sergio-2.local>
2026-09-25 23:27:11 +0200

    feat(api-call): enabled the core API for the auditor with FastAPI

---
+++ backend/app/api/endpoints.py
@@ -0,0 +1,29 @@
+import io
+from PIL import Image
+from fastapi import APIRouter, UploadFile, File, HTTPException, status
+from app.services.audit_service import FinancialAuditorService
+from app.schemas.schemas import FinancialExtraction, AuditResult
+MAXIMO_10_MB = 10 * 1024 * 1024
+
+router = APIRouter()
+auditor = FinancialAuditorService()
+
+@router.post("/audit", response_model=AuditResult)
+async def audit_document_endpoint(file: UploadFile =  File(...)):
+    if file.content_type not in ["image/jpeg", "image/png", "image/webp"]: #dejamos pdf para despues
+        raise HTTPException(
+        status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        detail=f"Formato '{file.content_type}' no soportado. Usa JPEG, PNG o WebP."
+    )
+
+    image_bytes = await file.read()
+
+    if len(image_bytes) > MAXIMO_10_MB:
+        raise HTTPException(
+        status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+        detail="El archivo supera el límite de 10 MB."
+    )
+    
+    resultado = auditor.audit_document(image_bytes)
+
+    return resultado
